b9df3008faf84464362cbdf0c623e22671816640
Liana RANDRIAMANANORO <lrandria@e1r8p21.clusters.42paris.fr>
2022-07-10 22:18:00 +0200

    cleaned up

--- ft_printf.c
+++ ft_printf.c
@@ -1,104 +1,92 @@
-/* Write a function named `ft_printf` that will mimic the real printf but 
-it will manage only the following conversions: s,d and x.
-
-Your function must be declared as follows:
-
-int ft_printf(const char *, ... );
-
-Before you start we advise you to read the `man 3 printf` and the `man va_arg`.
-To test your program compare your results with the true printf.
-
-Exemples of the function output:
- 
-call: ft_printf("%s\n", "toto");
-out: toto$
-
-call: ft_printf("Magic %s is %d", "number", 42);
-out: Magic number is 42%
-
-call: ft_printf("Hexadecimal for %d is %x\n", 42, 42);
-out: Hexadecimal for 42 is 2a$ */
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lrandria <lrandria@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/10 22:13:37 by lrandria          #+#    #+#             */
+/*   Updated: 2022/07/10 22:13:37 by lrandria         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdarg.h>
 
-typedef struct s_info
-{
-	int		nbytes;
-}				t_info;
+int g_ret;
 
-void	putnbr_base(t_info *t, long long num, char *str, int base)
+size_t	ft_strlen(char *s)
 {
-	long long	lol;
+	size_t	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
 
-	lol = num;
-	if (num < 0)
+void	ft_putnbr_base(long num, char *strbase, int base)
+{
+	long lol = num;
+	if (lol < 0)
 	{
 		lol = -lol;
-		t->nbytes += write(1, "-", 1);
+		g_ret += write(1, "-", 1);
 	}
 	if (lol >= base)
-		putnbr_base(t, lol / base, str, base);
-	t->nbytes += write(1, &str[lol % base], 1);
+		ft_putnbr_base(lol / base, strbase, base);
+	g_ret += write(1, &strbase[lol % base], 1);
 }
 
-void	print_s(t_info *t, va_list ap)
+void	print_s(va_list ap)
 {
-	size_t	i = 0;
 	char	*s = va_arg(ap, char *);
-	if (s == NULL)
-		t->nbytes += write(1, "(null)", 6);
-	else
-		while (s[i])
-			t->nbytes += write(1, &s[i++], 1);
+	g_ret += write(1, s, ft_strlen(s));
 }
 
-void	print_d(t_info *t, va_list ap)
+void	print_d(va_list ap)
 {
-	int d = va_arg(ap, int);
-	putnbr_base(t, d, "0123456789", 10);
+	int	d = va_arg(ap, int);
+	ft_putnbr_base(d, "0123456789", 10);
 }
 
-void	print_x(t_info *t, va_list ap)
+void	print_x(va_list ap)
 {
-	unsigned long x = va_arg(ap, unsigned long);
-	putnbr_base(t, x, "0123456789abcdef", 16);
+	unsigned int	x = va_arg(ap, unsigned int);
+	ft_putnbr_base(x, "0123456789abcdef", 16);
 }
 
-int	magic_kaboom(const char *str, t_info *t, va_list ap)
+void	convert(va_list ap, const char *s)
 {
 	size_t	i = 0;
-
-	while (str[i])
+	while (s[i])
 	{
-		if (str[i] == '%')
+		if (s[i] == '%')
 		{
-			if (str[i + 1] == 's')
-			 	print_s(t, ap);
-			else if (str[i + 1] == 'd')
-				print_d(t, ap);
-			else if (str[i + 1] == 'x')
-				print_x(t, ap);
+			if (s[i + 1] == 's')
+				print_s(ap);
+			else if (s[i + 1] == 'd')
+				print_d(ap);
+			else if (s[i + 1] == 'x')
+				print_x(ap);
+			else
+				g_ret += write(1, &s[i], 2);
 			i++;
 		}
 		else
-			t->nbytes += write(1, &str[i], 1);
+			g_ret += write(1, &s[i], 1);
 		i++;
-	 }
-	 return (t->nbytes);
+	}
 }
 
-int	ft_printf(const char *str, ...)
+int	ft_printf(const char *s, ...)
 {
-	va_list		ap;
-	t_info		t[1];
+	va_list	ap;
 
-	t->nbytes = 0;
-	va_start(ap, str);
-	magic_kaboom(str, t, ap);
+	g_ret = 0;
+	va_start(ap, s);
+	convert(ap, s);
 	va_end(ap);
-	return (t->nbytes);
+	return (g_ret);
 }
 
 #include <stdio.h>
@@ -106,22 +94,45 @@ int	ft_printf(const char *str, ...)
 
 int	main(void)
 {
-	printf("my ret = %d\n", ft_printf("Hello\n"));
-	printf("ret = %d\n", printf("Hello\n"));
-	printf("my ret = %d\n", ft_printf("%s\n", "U ok?"));
-	printf("ret = %d\n", printf("%s\n", "U ok?"));
-	printf("my ret = %d\n", ft_printf("%s\n", ""));
-	printf("ret = %d\n", printf("%s\n", "");
-	printf("my ret = %d\n", ft_printf("%d\n", 42));
-	printf("ret = %d\n", printf("%d\n", 42));
-	printf("my ret = %d\n", ft_printf("%x\n", 42));
-	printf("ret = %d\n", printf("%x\n", 42));
-	printf("my ret = %d\n", ft_printf("%s, %d, %x\n", "Hello", 42, 42));
-	printf("ret = %d\n", printf("%s, %d, %x\n", "Hello", 42, 42));
-	printf("my ret = %d\n", ft_printf("%d\n", 42));
-	printf("my ret = %d\n", ft_printf("%d\n", INT_MIN));
-	printf("ret = %d\n", printf("%d\n", INT_MIN));
-	printf("my ret = %d\n", ft_printf("%d\n", INT_MAX));
-	printf("ret = %d\n", printf("%d\n", INT_MAX));
-	return (0);
+	printf("MINE: %d\n", ft_printf("HI THERE "));
+	printf("REAL: %d\n", printf("HI THERE "));
+	printf("============================================\n");
+	printf("MINE: %d\n", ft_printf("[%s] ", "COUCOU"));
+	printf("REAL: %d\n", printf("[%s] ", "COUCOU"));
+	printf("============================================\n");
+	printf("MINE: %d\n", ft_printf("%s", ""));
+	printf("REAL: %d\n", printf("%s", ""));
+	printf("============================================\n");
+	printf("MINE: %d\n", ft_printf("[%d] ", 42));
+	printf("REAL: %d\n", printf("[%d] ", 42));
+	printf("============================================\n");
+	printf("MINE: %d\n", ft_printf("[%d] ", INT_MIN));
+	printf("REAL: %d\n", printf("[%d] ", INT_MIN));
+	printf("============================================\n");
+	printf("MINE: %d\n", ft_printf("[%d] ", INT_MAX));
+	printf("REAL: %d\n", printf("[%d] ", INT_MAX));
+	printf("============================================\n");
+	printf("MINE: %d\n", ft_printf("[%d] ", LONG_MIN));
+	printf("REAL: %d\n", printf("[%d] ", LONG_MIN));
+	// printf("============================================\n");
+	// printf("MINE: %d\n", ft_printf("[%d] ", LONG_MAX));
+	// printf("REAL: %d\n", printf("[%d] ", LONG_MAX));
+	printf("============================================\n");
+	printf("MINE: %d\n", ft_printf("[%x] ", 42));
+	printf("REAL: %d\n", printf("[%x] ", 42));
+	printf("============================================\n");
+	printf("MINE: %d\n", ft_printf("[%x] ", UINT_MAX));
+	printf("REAL: %d\n", printf("[%x] ", UINT_MAX));
+	printf("============================================\n");
+	printf("MINE: %d\n", ft_printf("[%x] ", ""));
+	printf("REAL: %d\n", printf("[%x] ", ""));
+	printf("============================================\n");
+	printf("MINE: %d\n", ft_printf("[%x] ", ULONG_MAX));
+	printf("REAL: %d\n", printf("[%x] ", ULONG_MAX));
+	printf("============================================\n");
+	printf("MINE: %d\n", ft_printf("Hello: [%s] [%d] [%x] ", "moshimoshi", 255, 255));
+	printf("REAL: %d\n", printf("Hello: [%s] [%d] [%x] ", "moshimoshi", 255, 255));
+	printf("============================================\n");
+	printf("MINE: %d\n", ft_printf("Hello: [%s] [%d] [%x]", "weiwei", 400, 400));
+	printf("REAL: %d\n", printf("Hello: [%s] [%d] [%x]", "weiwei", 400, 400));
 }
--- get_next_line.c
+++ get_next_line.c
@@ -1,84 +1,70 @@
-/* Write a function named get_next_line which prototype should be:
-char *get_next_line(int fd);
-
-Your function must return a line that has been read from the file descriptor passed as
-parameter. What we call a "line that has been read" is a succesion of 0 to n 
-characters that end with '\n' (ascii code 0x0a) or with End Of File (EOF).
-The line should be returned including the '\n' in case there is one at the end of the
-line that has been read. When you've reached the EOF, you must store the current
-buffer in a char * and return it. If the buffer is empty you must return NULL.
-
-In case of error return NULL. In case of not returning NULL, the pointer should be
-free-able. Your program will be compiled with the flag -D BUFFER_SIZE=xx, which has to
-be used as the buffer size for the read calls in your functions.
-
-Your function must be memory leak free. When you've reached the EOF, your function
-should keep 0 memory allocated with malloc, except the line that has been returned.
-
-Caling your function get_next_line in a lop will therefore allow you to read the text
-avaiable on a file descriptor one line at a time until the end of the text, no matter
-the size of either the text or one of its lines.
-
-Make sure that your function behaves wel when it reads from a file, from the standard
-output, from a redirection, etc...
-
-No call to another function will be done on the file descriptor between 2 calls of
-get_next_line. Finally we consider that get_next_line has an undefined behaviour when
-reading from a binary file.*/
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lrandria <lrandria@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/10 22:16:17 by lrandria          #+#    #+#             */
+/*   Updated: 2022/07/10 22:16:17 by lrandria         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdlib.h>
-#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <fcntl.h>
 
-char    *get_next_line(int fd)
-{
-    char    stock[10000];
-    char    buf[1];
-    char    *line;
-    int        i;
+// gcc -Wall -Wextra -Werror -fsanitize=address -g3 -D BUFFER_SIZE=whatever
 
-    i = 0;
-    if (fd < 0 || BUFFER_SIZE <= 0){
-        return (NULL);}
+char	*get_next_line(int fd)
+{
+	size_t	i;
+	char	*line;
+	char	buf[1];
+	char	stock[999999];
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	i = 0;
 	stock[i] = '\0';
-    while (read(fd, buf, 1) == 1)
-    {
-        stock[i] = buf[0];
-        stock[i + 1] = '\0';
-        if (stock[i] == '\n')
-            break ;
-        i++;
-    }
-    if (stock[0] == '\0'){
-		return (NULL);}
-    line = malloc(i + 2);
-    i = 0;
-    while (stock[i])
-    {
-        line[i] = stock[i];
-        i++;
-    }
-    line[i] = '\0';
-    return (line);
+	while (read(fd, buf, 1) == 1)
+	{
+		stock[i] = buf[0];
+		stock[i + 1] = '\0';
+		if (stock[i] == '\n')
+			break ;
+		i++;
+	}
+	if (stock[0] == '\0')
+		return (NULL);
+	line = malloc(i + 2);
+	i = 0;
+	while (stock[i])
+	{
+		line[i] = stock[i];
+		i++;
+	}
+	line[i] = '\0';
+	return (line);
 }
 
-int main(void)
+#include <stdio.h>
+
+int	main(void)
 {
 	char	*line;
 	int		fd = 0;
 	int		loop = 1;
 
-	fd = open("test.txt", O_RDONLY);
+	fd =  open("test.txt", O_RDONLY);
 	if (fd == -1)
-	{
-		printf("Error while opening file\n");
 		return (1);
-	}
 	while (loop)
 	{
 		line = get_next_line(fd);
-		if (line != NULL)
+		if (line)
 		{
 			printf("%s", line);
 			free(line);
